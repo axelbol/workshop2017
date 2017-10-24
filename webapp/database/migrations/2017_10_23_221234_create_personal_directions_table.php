09f27ae555b31b229ac63b7e69db2f20a03afd4d
cesarmurillo <csrtmt@gmail.com>
2017-10-23 22:30:49 -0400

    Adding migration, model and controller for directions related to user

---
+++ webapp/database/migrations/2017_10_23_221234_create_personal_directions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonalDirectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_directions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('country');
+            $table->string('state');
+            $table->string('city');
+            $table->string('address');
+            $table->integer('postal_code');
+            $table->integer('phone_number');
+            $table->integer('cellular_number');
+            $table->boolean('active');
+
+            $table->integer('account_id')->unsigned();
+            $table->foreign('account_id')->references('id')->on('personal_accounts');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal_directions');
+    }
+}
