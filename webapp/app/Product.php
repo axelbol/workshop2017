a89099bc146db6d61cbcc3a4951f6ede8eaa855b
cesarmurillo <csrtmt@gmail.com>
2017-10-26 03:01:35 -0400

    Product controller now is updated to work with new features
    The Store method in ProductController has been modified to work with new features like the dynamic forms

--- webapp/app/Product.php
+++ webapp/app/Product.php
@@ -9,10 +9,18 @@ class Product extends Model
 {
     use SoftDeletes;
     protected $dates = ['deleted_at'];
-    protected $fillable = ['title', 'subtitle', 'category', 'condition', 'description'];
+    protected $fillable = ['title', 'subtitle', 'isbn', 'condition', 'description'];
 
     public function personalaccount()
     {
         return $this->belongsTo('App\PersonalAccount');
+    }
+    public function productphotos()
+    {
+        return $this->hasMany('App\ProductPhoto');
+    }
+    public function features()
+    {
+        return $this->hasMany('App\Feature');
     }
 }
