f652b88bd690b3b7cb9a9818239697ef7b6ca401
axelbol <axel_lp_alvarado@hotmail.com>
2017-10-29 20:10:48 -0400

    migration and model supplier

---
+++ webapp/database/migrations/2017_10_29_121909_create_suppliers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('companyName');
+            $table->string('contacName');
+            $table->string('address');
+            $table->string('product');
+            $table->string('phono');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
