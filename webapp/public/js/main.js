e8845838926802e0d556b23fe5f887f68236031c
cesarmurillo <csrtmt@gmail.com>
2017-11-06 23:18:20 -0400

    Added some functions

--- webapp/public/js/main.js
+++ webapp/public/js/main.js
@@ -14,11 +14,18 @@ $(document).ready(function() {
 
     $("#btn1").click(function(){
 
-        $("x").append("<input id='spec"+aa+"'  name='feature[]' class='form-control' type='text' readonly value='"+$('#trait').val()+"'>");
-        $("y").append("<input id='spec"+aa+"' name='desc[]' class='form-control' type='text' readonly value='"+$('#desc').val()+"'>");
-        $("z").append("");
+        $("x").append("<input id='"+aa+"'  name='feature[]' class='form-control' type='text' readonly value='"+$('#trait').val()+"'>");
+        $("y").append("<input id='"+aa+"' name='desc[]' class='form-control' type='text' readonly value='"+$('#desc').val()+"'>");
+        $("z").append("<button id='btn2' data-id='"+aa+"' class='form-control'>Eliminar</button>");
         aa++;
     });
+    $("z").on('click', '#btn2', function(){
 
+        var dataId = $(this).data("id");
+        $("#"+dataId).remove();
+        $("#"+dataId).remove();
+        $("#btn2").filter("[data-id="+dataId+"]").remove();
+
+    });
 
 });
